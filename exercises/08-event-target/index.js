024c3edc14b9579db5fae5d040f57a6fb634d5a3
SilMontes <Silmontes2001@gmail.com>
2021-02-26 17:51:53 +0000

    Done

--- exercises/08-event-target/index.js
+++ exercises/08-event-target/index.js
@@ -1,8 +1,8 @@
 window.onload = function loadFn() {
 	var containerElm = document.getElementById("container");
 	containerElm.addEventListener("click", function(event) {
-        //you code here
-        
-		alert(event.target.tagId);
+		//you code here
+		var x = event.target.getAttribute("id");
+		alert(x);
 	});
 };
